a077ddd4103ea7a06c9f9fc65a00af1d6e7e7aa6
JayGangkun <jaygangkun@hotmail.com>
2021-12-14 21:49:38 -0500

    homepage is done

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function($){
+    $(function() { 
+        $('#home_sections').fullpage({
+            licenseKey: 'A18DEEC0-91C048BF-BD770CDA-2BFC65D0',
+            autoScrolling: true,
+            scrollHorizontally: true,
+            scrollOverflow: true
+        });
+    })
+})(jQuery)
+
