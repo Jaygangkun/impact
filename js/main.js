e1953e8b33183a0d0833456e07f732212c24fda7
JayGangkun <jaygangkun@hotmail.com>
2021-12-16 20:01:20 -0500

    homepage parallax

--- js/main.js
+++ js/main.js
@@ -5,8 +5,19 @@
                 licenseKey: 'A18DEEC0-91C048BF-BD770CDA-2BFC65D0',
                 autoScrolling: true,
                 scrollHorizontally: true,
-                scrollOverflow: true
+                scrollOverflow: true,
+                normalScrollElements: '.home-section-wrap',
             });
+
+            $(document).on('click', '.home-section .home-section-expand .btn-learn-more', function() {
+                $(this).parents('.home-section').addClass('expanded');
+                $(this).parents('.home-section').find('.home-section-expand-content').slideDown();
+            })
+
+            $(document).on('click', '.home-section .home-section-expand .btn-collapse-times', function() {
+                $(this).parents('.home-section').removeClass('expanded');
+                $(this).parents('.home-section').find('.home-section-expand-content').slideUp();
+            })
         }
 
         if($('.page-projects').length > 0) {
