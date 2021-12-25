83c665ee78a9a9972b91283570cfe87f41d2a2c2
JayGangkun <jaygangkun@hotmail.com>
2021-12-24 20:21:31 -0500

    fix issue

--- js/main.js
+++ js/main.js
@@ -13,10 +13,130 @@
         if($('.page-home').length > 0) {
             $('#home_sections').fullpage({
                 licenseKey: 'A18DEEC0-91C048BF-BD770CDA-2BFC65D0',
-                autoScrolling: true,
-                scrollHorizontally: true,
+                // scrollHorizontally: true,
                 scrollOverflow: true,
-                normalScrollElements: '.home-section-wrap',
+                normalScrollElements: '.home-section',
+            });
+
+            // $(".home-section").scroll(function() { //.box is the class of the div
+            //     console.log('scroll');
+            // });
+
+            let curFPSection = '';
+            const FPSections = {
+                'home_section_impact': {
+                    'prev': '',
+                    'next': 'home_section_healthy_living',
+                    'index': 1
+                },
+                'home_section_healthy_living': {
+                    'prev': 'home_section_impact',
+                    'next': 'home_section_educational',
+                    'index': 2
+                },
+                'home_section_educational': {
+                    'prev': 'home_section_healthy_living',
+                    'next': 'home_section_placemaking',
+                    'index': 3
+                },
+                'home_section_placemaking': {
+                    'prev': 'home_section_educational',
+                    'next': 'home_section_economic',
+                    'index': 4
+                },
+                'home_section_economic': {
+                    'prev': 'home_section_placemaking',
+                    'next': 'home_section_footer',
+                    'index': 5
+                },
+                'home_section_footer': {
+                    'prev': 'home_section_economic',
+                    'next': '',
+                    'index': 6
+                }
+            }
+
+            function moveFPSection(id, dir) {
+                console.log("moveFPSection >>", id, dir);
+                
+                var desSectionID = FPSections[id][dir];
+                if(dir == 'prev' && curFPSection == 'home_section_footer') {
+                    desSectionID = 'home_section_economic';
+                }
+                console.log("desSectionID >>", desSectionID);
+                curFPSection = desSectionID;
+                if(desSectionID != '') {
+                    fullpage_api.moveTo(FPSections[desSectionID]['index']);
+                }
+                
+            }
+
+            $('.home-section').on('swipedown',function() {
+                console.log('swipdown');
+            });
+
+            $('.home-section').on('swipeup',function() {
+                console.log('swipup');
+            });
+
+            $('.home-section').on('swipe',function() {
+                console.log('swipe');
+            });
+
+            $('.home-section').bind('mousewheel', function (e) {
+                console.log( $(this).find('.home-section-content').offset().top, $(this).find('.home-section-content').outerHeight(), $(window).height());
+                console.log(Math.floor($(this).find('.home-section-content').offset().top + $(this).find('.home-section-content').outerHeight() + 60), Math.floor($(window).height()));
+
+                if(e.originalEvent.wheelDelta > 0) {
+                    direction = 'up';
+                    if(Math.floor($(this).find('.home-section-content').offset().top + $(this).find('.home-section-content').outerHeight() + 61) < Math.floor($(window).height())) {
+                        console.log('prev1');
+                        moveFPSection($(this).attr('id'), 'prev');
+                        
+                    }
+                    else if(Math.floor($(this).find('.home-section-content').offset().top) > 59) {
+                        console.log('prev2');
+                        moveFPSection($(this).attr('id'), 'prev');
+                        
+                    }
+                    else {
+                        console.log('scroll1');
+                    }
+
+                }
+                else{
+                    if(Math.floor($(this).find('.home-section-content').offset().top + $(this).find('.home-section-content').outerHeight() + 60) < Math.floor($(window).height())) {
+                        console.log('next1');
+                        moveFPSection($(this).attr('id'), 'next');
+                    }
+                    else {
+                        console.log('scroll2');
+                    }
+                }
+
+                // if(Math.floor($(this).find('.home-section-content').offset().top + $(this).find('.home-section-content').outerHeight() + 60) < Math.floor($(window).height())) {
+                    
+                //     if(direction == 'down') {
+                //         console.log('next1');
+                //     }
+                //     else {
+                //         console.log('prev1');
+                //     }
+                    
+                //     // fullpage_api.moveSectionDown();
+                // }
+                // else if(Math.floor($(this).find('.home-section-content').offset().top) > 59) {
+                //     if(direction == 'up') {
+                //         console.log('prev2');
+                //     }
+                //     else {
+                //         console.log('scroll2');
+                //     }
+                    
+                // }
+                // else {
+                //     console.log('scroll3');
+                // }
             });
 
             $(document).on('click', '.home-section .home-section-expand .btn-learn-more', function() {
